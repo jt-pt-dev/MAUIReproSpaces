84e5fd0434e2b4bc07c23397517d6ad6fb279b2b
Jason Taylor <jason.taylor@pertrain.com.au>
2022-08-17 13:43:12 +1000

    Add project files.

---
+++ MAUIReproSpaces/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUIReproSpaces;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MAUIReproSpaces/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUIReproSpaces;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
